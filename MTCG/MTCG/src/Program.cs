cb968670967a5f0282138ddd7aad117c14732136
Bernhard Weidinger <weidinger.bernhard@student.htlwrn.ac.at>
2021-11-09 18:54:08 +0100

    Create console application

---
+++ MTCG/MTCG/src/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MTCG {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
