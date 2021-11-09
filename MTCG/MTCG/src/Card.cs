4a0d9c2d73c6b132ee8b0dd3a87e1b1dc7810cce
Bernhard Weidinger <weidinger.bernhard@student.htlwrn.ac.at>
2021-11-09 20:12:06 +0100

    Add temporary test code to main

--- MTCG/MTCG/src/Card.cs
+++ MTCG/MTCG/src/Card.cs
@@ -6,10 +6,10 @@ namespace MTCG.src {
     public abstract class Card {
         public Guid id { get; private set; }
         public string name { get; private set; }
-        public int damage { get; private set; }
+        public double damage { get; private set; }
         public ElementType elementType { get; private set; }
 
-        public Card(Guid id, string name, int damage) {
+        public Card(Guid id, string name, double damage) {
             this.id = id;
             this.name = name;
             this.damage = damage;
@@ -27,19 +27,18 @@ namespace MTCG.src {
     public class MonsterCard : Card {
         public MonsterType monsterType { get; private set; }
 
-        public MonsterCard(Guid id, string name, int damage) : base(id, name, damage) {
+        public MonsterCard(Guid id, string name, double damage) : base(id, name, damage) {
             string monsterName = name;
             if (elementType == ElementType.fire) {
                 monsterName = name.Substring(4);
             } else if (elementType == ElementType.water) {
                 monsterName = name.Substring(5);
             }
-
-            monsterType = (MonsterType)System.Enum.Parse(typeof(MonsterType), monsterName);
+            monsterType = (MonsterType)System.Enum.Parse(typeof(MonsterType), monsterName.ToLower());
         }
     }
 
     public class SpellCard : Card {
-        public SpellCard(Guid id, string name, int damage) : base(id, name, damage) { }
+        public SpellCard(Guid id, string name, double damage) : base(id, name, damage) { }
     }
 }
--- MTCG/MTCG/src/Program.cs
+++ MTCG/MTCG/src/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using MTCG.src;
 
 namespace MTCG {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            MonsterCard m1 = new MonsterCard(new Guid(), "Goblin", 10.0);
+            SpellCard s1 = new SpellCard(new Guid(), "RegularSpell", 10.0);
+            SpellCard s2 = new SpellCard(new Guid(), "WaterSpell", 10.0);
+
+            Console.WriteLine(m1.elementType + " " + m1.monsterType);
+            Console.WriteLine(s1.elementType);
+            Console.WriteLine(s2.elementType);
             Console.Read();
         }
     }
