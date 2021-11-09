1acae263df924aaa9f878147229b3871e4febf2d
Bernhard Weidinger <weidinger.bernhard@student.htlwrn.ac.at>
2021-11-09 19:52:57 +0100

    Add enums to project

---
+++ MTCG/MTCG/src/enums.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTCG.src {
+    public enum ElementType {
+        fire,
+        water,
+        normal
+    }
+
+    public enum MonsterType {
+        goblin,
+        dragon,
+        wizard,
+        ork,
+        knight,
+        kraken,
+        elf
+    }
+
+    public enum CardType {
+        monster,
+        spell
+    }
+}
