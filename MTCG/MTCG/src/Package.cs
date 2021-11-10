b47fc99343e301f3efe9b3e96daec81c369a8fff
Bernhard Weidinger <weidinger.bernhard@student.htlwrn.ac.at>
2021-11-10 08:24:40 +0100

    Add class Package to project and implement functions

---
+++ MTCG/MTCG/src/Package.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTCG.src {
+    public class Package {
+        public Guid id { get; private set; }
+        public List<Card> cards { get; private set; }
+
+        public Package(List<Card> cards) {
+            if (cards.Count != 5) {
+                throw new ArgumentException($"A package should be provided with 5 cards, " +
+                    $"cards given: {cards.Count}");
+            } else {
+                this.cards = cards;
+            }
+        }
+
+        public void aquirePackage(User user) {
+            user.stack.AddRange(cards);
+        }
+    }
+}
