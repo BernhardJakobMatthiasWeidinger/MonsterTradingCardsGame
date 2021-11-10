e7594b59828b0b8c10e26cf360394c499efd4905
Bernhard Weidinger <weidinger.bernhard@student.htlwrn.ac.at>
2021-11-10 09:46:35 +0100

    Add temporary test code and fixed bugs in class Trade

--- MTCG/MTCG/src/Program.cs
+++ MTCG/MTCG/src/Program.cs
@@ -5,23 +5,37 @@ using MTCG.src;
 namespace MTCG {
     class Program {
         static void Main(string[] args) {
-            //Initialize cards
+            //Create cards
+            Console.WriteLine("Create Cards:");
+            try {
+                MonsterCard m0 = new MonsterCard(Guid.NewGuid(), "Unicorn", 10.0);
+            } catch (ArgumentException e) {
+                Console.WriteLine(e.Message);
+            }
+
             MonsterCard m1 = new MonsterCard(Guid.NewGuid(), "Ork", 10.0);
             MonsterCard m2 = new MonsterCard(Guid.NewGuid(), "FireElf", 12.0);
             MonsterCard m3 = new MonsterCard(Guid.NewGuid(), "WaterDragon", 25.0);
+            MonsterCard m4 = new MonsterCard(Guid.NewGuid(), "WaterDragon", 34.0);
+            MonsterCard m5 = new MonsterCard(Guid.NewGuid(), "FireKraken", 20.0);
             SpellCard s1 = new SpellCard(Guid.NewGuid(), "RegularSpell", 14.0);
             SpellCard s2 = new SpellCard(Guid.NewGuid(), "WaterSpell", 20.0);
             SpellCard s3 = new SpellCard(Guid.NewGuid(), "FireSpell", 30.0);
+            SpellCard s4 = new SpellCard(Guid.NewGuid(), "WaterSpell", 25.0);
+            SpellCard s5 = new SpellCard(Guid.NewGuid(), "RegularSpell", 40.0);
 
-            //Initialize packages
+            //Create packages
+            Console.WriteLine("\nCreate Packages:");
             try {
                 Package p0 = new Package(new List<Card> { m1, m2, s1, s2 });
             } catch (ArgumentException e) {
                 Console.WriteLine(e.Message);
             }
             Package p1 = new Package(new List<Card> { m1, m2, m3, s1, s2 });
+            Package p2 = new Package(new List<Card> { m4, m5, s3, s4, s5 });
 
-            //Initialize user
+            //Create user1
+            Console.WriteLine("\nCreate User 1 and add cards to stack and deck:");
             User u1 = new User("maxiiii", "supersecretpassword1");
             p1.aquirePackage(u1);
             try {
@@ -37,9 +51,50 @@ namespace MTCG {
 
             u1.configureDeck(new List<Guid> { m1.id, m2.id, m3.id, s1.id });
 
+            //Create user2
+            User u2 = new User("miniiii", "supersecretpassword2");
+            p2.aquirePackage(u2);
+            u2.configureDeck(new List<Guid> { m4.id, m5.id, s3.id, s4.id });
+
+            //Create Trade
+            Console.WriteLine("\nCreate Trade:");
+            try {
+                Trade tfail1 = new Trade(Guid.NewGuid(), s4, u1, CardType.spell, ElementType.fire, 15.0);
+            } catch (ArgumentException e) {
+                Console.WriteLine(e.Message);
+            }
+            try {
+                Trade tfail2 = new Trade(Guid.NewGuid(), s1, u1, CardType.spell, ElementType.fire, 15.0);
+            } catch (ArgumentException e) {
+                Console.WriteLine(e.Message);
+            }
+
+            Trade t1 = new Trade(Guid.NewGuid(), s2, u1, CardType.spell, ElementType.normal, 15.0);
+
+            //Trade with user2
+            Console.WriteLine("\nTrade with user2:");
+            try {
+                t1.TradeCard(u1, s5);
+            } catch (ArgumentException e) {
+                Console.WriteLine(e.Message);
+            }
+            try {
+                t1.TradeCard(u2, s4);
+            } catch (ArgumentException e) {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Before Trade:");
+            Console.WriteLine("User1 has card s5 in stack: " + u1.stack.Contains(s5));
+            Console.WriteLine("User2 has card s2 in stack: " + u2.stack.Contains(s2));
+            t1.TradeCard(u2, s5);
+            Console.WriteLine("After Trade:");
+            Console.WriteLine("User1 has card s5 in stack: " + u1.stack.Contains(s5));
+            Console.WriteLine("User2 has card s2 in stack: " + u2.stack.Contains(s2));
+
             //get 100 times a random card from the deck
             for (int i=0; i < 100; ++i) {
-                Console.WriteLine(u1.getCardFromDeck().name);
+                Console.WriteLine(u2.getCardFromDeck().name);
             }
             Console.Read();
         }
--- MTCG/MTCG/src/Trade.cs
+++ MTCG/MTCG/src/Trade.cs
@@ -9,30 +9,39 @@ namespace MTCG.src {
         public User user { get; private set; }
         public CardType cardType { get; private set; }
         public ElementType elementType { get; private set; }
-        public int minimumDamage { get; private set; }
+        public double minimumDamage { get; private set; }
 
         public Trade(Guid id, Card cardToTrade, User user, CardType cardType, 
-            ElementType elementType, int minimumDamage) {
+            ElementType elementType, double minimumDamage) {
             this.id = id;
-            this.cardToTrade = cardToTrade;
             this.user = user;
             this.cardType = cardType;
             this.elementType = elementType;
             this.minimumDamage = minimumDamage;
+
+            if (user.deck.Contains(cardToTrade)) {
+                throw new ArgumentException("Cannot trade card if it's in the deck!");
+            } else if (!user.stack.Contains(cardToTrade)) {
+                throw new ArgumentException("Cannot trade card if it's not in the stack!");
+            } else {
+                this.cardToTrade = cardToTrade;
+            }
         }
 
         public void TradeCard(User u2, Card cardForTrade) {
-            if (cardForTrade.GetType().Name == "MonsterCard" && cardType == CardType.spell ||
+            if (user == u2) {
+                throw new ArgumentException("Cannot trade card with yourself!");
+            } else if (u2.deck.Contains(cardForTrade)) {
+                throw new ArgumentException("Cannot trade card if it's in the deck!");
+            } else if (!u2.stack.Contains(cardForTrade)) {
+                throw new ArgumentException("Cannot trade card if it's not in the stack!");
+            } else if (cardForTrade.GetType().Name == "MonsterCard" && cardType == CardType.spell ||
                 cardForTrade.GetType().Name == "SpellCard" && cardType == CardType.monster) {
                 throw new ArgumentException("Cannot trade card, wrong card type was provided!");
             } else if (this.elementType != cardForTrade.elementType) {
                 throw new ArgumentException("Cannot trade card, wrong element type was provided!");
             } else if (this.minimumDamage > cardForTrade.damage) {
                 throw new ArgumentException("Cannot trade card, damage of card is too small!");
-            } else if (u2.deck.Contains(cardForTrade)) {
-                throw new ArgumentException("Cannot trade card if it's in the deck!");
-            } else if (!u2.stack.Contains(cardForTrade)) {
-                throw new ArgumentException("Cannot trade card if it's not in the stack!");
             }
 
             //trade provided card
